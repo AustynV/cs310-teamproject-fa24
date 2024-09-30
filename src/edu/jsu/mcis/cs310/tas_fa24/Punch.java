92066640811a0e9aead90c2ab87d5c624cfd5614
Cayden Tucker <ctucker18@stu.jsu.edu>
2024-09-30 12:13:57 -0500

    added a few lines of code

--- src/edu/jsu/mcis/cs310/tas_fa24/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/Punch.java
@@ -4,7 +4,26 @@
  */
 package edu.jsu.mcis.cs310.tas_fa24;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
 
 public class Punch {
+    private int id;
+    private final int terminalId;
+    private final Badge badge;
+    private final LocalDateTime originaltimestamp;
+    private LocalDateTime adjustedtimestamp;
+    private final EventTime punchtype;
+    priavte PunchAdjustmentType adjustmenttype;
+    private LunchStatus adjustedlunchstatus;
     
+    public enum LunchStatus {
+        HAPPENING, HAPPENED, NOT_HAPPENING, INAPPLICABLE
+    }
+    
+    public Punch(){
+        
+    }
 }
