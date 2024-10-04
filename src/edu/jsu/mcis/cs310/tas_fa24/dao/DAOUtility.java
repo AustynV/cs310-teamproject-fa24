895346456e8038949c6001deb77d83a58314c137
AustynV <Avincent5@stu.jsu.edu>
2024-10-04 11:37:37 -0500

    Worked on time accrued method

--- src/edu/jsu/mcis/cs310/tas_fa24/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/dao/DAOUtility.java
@@ -14,5 +14,38 @@ import com.github.cliftonlabs.json_simple.*;
  * 
  */
 public final class DAOUtility {
+    public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift){
+        
+    int totalMinutes = 0;
+    boolean isLunchDeducted = false;
 
+    // Checks if punch list has a even number of punches, because their needs to be pairs of in and out punches.
+    if(dailypunchlist.size() < 2){
+        // No complete pair to compute time.
+        return 0;
+    }
+
+    for(int i = 0; i < dailypunchlist.size() - 1; i += 2){
+        Punch punchIn = dailypunchlist.get(i);
+        Punch punchOut = dailypunchlist.get(i + 1);
+
+        // Ignores the "time out" punches that occur.
+        if(punchOut.getPunchtype() == Punch.PunchType.TIME_OUT){
+            continue;
+        }
+
+        // Calculates the duration between the clock in and clock out punches.
+        Duration duration = Duration.between(punchIn.getAdjustedTimestamp(), punchOut.getAdjustedTimestamp());
+        totalMinutes += duration.toMinutes();
+    }
+
+    // If they have a lunch break, then it deducts it form their total time (totalMinutes).
+    if(totalMinutes > shift.getLunchThreshold()){
+        totalMinutes -= shift.getLunchDuration();
+        isLunchDeducted = true;
+    }
+    
+    // Returns value for totalMinutes back to variable.
+    return totalMinutes;
+}
 }
