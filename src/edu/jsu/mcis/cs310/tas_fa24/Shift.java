0ca8c25ceac610d74598c54f9df4ee2e1e7c544c
Cayden Tucker <ctucker18@stu.jsu.edu>
2024-10-14 11:16:15 -0500

    Added some methods, should pass now

--- src/edu/jsu/mcis/cs310/tas_fa24/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/Shift.java
@@ -17,8 +17,34 @@ public class Shift {
     public Shift(Map<String, String> parameters){
         this.startTime = parameters.get("startTime");
         this.stopTime = parameters.get("stopTime");
-        this.otherParameter = parameters.get("otherParameters");
+        this.otherParameter = parameters.get("otherParameter");
         this.lunchDuration = Integer.parseInt(parameters.get("lunchDuration"));
         this.shiftDuration = Integer.parseInt(parameters.get("shiftDuration"));
+    }
+    
+    public String getStarted(){
+        return startTime;
+    }
+    
+    public String getStopTime(){
+        return stopTime;
+    }
+    
+    public String getOtherParameters(){
+        return otherParameter;
+    }
+    
+    public int getLunchDuration(){
+        return lunchDuration;
+    }
+    
+    public int getShiftDuration(){
+        return shiftDuration;
+    }
+    
+    public String toString(){
+        return "Shift{" + "startTime='" + startTime + '\'' + ", stopTime='" + stopTime + '\'' +
+                ", otherParameter='" + otherParameter + '\'' + ", lunchDuration=" + lunchDuration + 
+                ", shiftDuration=" + shiftDuration +'}';
     }
 }
