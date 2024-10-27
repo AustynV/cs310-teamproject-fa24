8b6823de4b5e5d1cc75878b4893811f2720ad4bd
CaydenT <ctucker18@stu.jsu.edu>
2024-10-26 21:31:51 -0500

    tried to do different experiments with the code, changed them back

--- test/edu/jsu/mcis/cs310/tas_fa24/PunchCreateTest.java
+++ test/edu/jsu/mcis/cs310/tas_fa24/PunchCreateTest.java
@@ -14,20 +14,17 @@ public class PunchCreateTest {
     public void setup() {
 
         daoFactory = new DAOFactory("tas.jdbc");
-
     }
 
     @Test
     public void testCreatePunch1() {
-
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
         PunchDAO punchDAO = daoFactory.getPunchDAO();
         BadgeDAO badgeDAO = daoFactory.getBadgeDAO();
 
         /* Create New Punch Object */
         
-        Punch p1 = new Punch(103, badgeDAO.find("021890C0"), EventType.CLOCK_IN);
+            Punch p1 = new Punch(103, badgeDAO.find("021890C0"), EventType.CLOCK_IN);
 
         /* Create Timestamp Objects */
         
@@ -35,29 +32,30 @@ public class PunchCreateTest {
 
         /* Get Punch Properties */
         
-        String badgeid = p1.getBadge().getId();
-        ots = p1.getOriginaltimestamp();
-        int terminalid = p1.getTerminalid();
-        EventType punchtype = p1.getPunchtype();
+            String badgeid = p1.getBadge().getId();
+            ots = p1.getOriginaltimestamp();
+            
+            int terminalid = p1.getTerminalid();
+            EventType punchtype = p1.getPunchtype();
 
         /* Insert Punch Into Database */
         
-        int punchid = punchDAO.create(p1);
+            int punchid = punchDAO.create(p1);
 
         /* Retrieve New Punch */
         
-        Punch p2 = punchDAO.find(punchid);
+            Punch p2 = punchDAO.find(punchid);
 
         /* Compare Punches */
         
-        assertEquals(badgeid, p2.getBadge().getId());
+            assertEquals(badgeid, p2.getBadge().getId());
 
         rts = p2.getOriginaltimestamp();
 
-        assertEquals(terminalid, p2.getTerminalid());
-        assertEquals(punchtype, p2.getPunchtype());
-        assertEquals(ots.format(dtf), rts.format(dtf));
+            assertEquals(terminalid, p2.getTerminalid());
+            assertEquals(punchtype, p2.getPunchtype());
+            assertEquals(ots.format(dtf), rts.format(dtf));
 
-    }
+        }
 
 }
